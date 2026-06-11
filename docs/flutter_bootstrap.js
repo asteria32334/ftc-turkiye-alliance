39806ee47092eaeeda8815ee08656f5afbef606e
asteria32334 <asteria.32334@gmail.com>
2026-06-11 16:15:52 +0000

    Clean deploy - remove submodule issue

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2129533175" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "465463153" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
