26d673d68f6d27fd6a6d1bc685d2ec86008c324d
asteria32334 <asteria.32334@gmail.com>
2026-06-12 08:39:01 +0000

    Fix white screen - base href final

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1768959253" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "939179246" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
