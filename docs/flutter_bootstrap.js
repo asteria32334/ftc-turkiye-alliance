a1a5eec6d7a1c1493debfbf8aecedd584f2adab3
asteria32334 <asteria.32334@gmail.com>
2026-06-12 06:04:17 +0000

    Final Deploy - FTC Türkiye Alliance

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "465463153" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1198452237" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
